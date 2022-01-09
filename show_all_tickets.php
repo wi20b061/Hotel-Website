3192c016ec8f682f6a4e08317f47254bea290e90
NadiiaGoncharenko <83373973+NadiiaGoncharenko@users.noreply.github.com>
2022-01-09 14:05:47 +0100

    Show all tickets /
    wenn Variable umbenannt sind - auch Users

---
+++ show_all_tickets.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>All tickets</title>
+</head>
+<body>
+
+    <?php
+        require_once ('dbaccess.php');
+
+        $db_obj = new mysqli($host, $user, $password, $database);
+        if ($db_obj->connect_error) {
+            echo "Connection Error: " . $db_obj->connect_error;
+            exit();
+        }
+
+        $sql = "SELECT * FROM tickets";
+        $stmt = $db_obj->prepare($sql);
+        $stmt->execute();
+        $stmt->bind_result($ticketID, $file_path, $title, $comment, $userID);
+    ?>
+
+    <h1>Tickets</h1>
+    <table border="1">
+        <th>Ticket ID</th>
+        <th>User ID</th>
+        <th>Title</th>
+        <th>Comment</th>
+        <th>Picture</th>
+
+        <?php
+            while ($stmt->fetch()) {
+                echo "<tr>";
+                echo "<td>" . $ticketID . "</td>";
+                echo "<td>" . $userID . "</td>";
+                echo "<td>" . $title . "</td>";
+                echo "<td>" . $status . "</td>";
+                echo "<td>" . $text . "</td>";
+                echo "<td>" . $comment . "</td>";
+                echo "<td><a href='" . $file_path . "' target='_blank'><img src='$file_path' alt='picture' height='50'></a>". "</td>";
+                echo "</tr>";
+            }
+            $stmt->close();
+            $db_obj->close();
+        ?>
+    </table>
+
+</body>
+</html>

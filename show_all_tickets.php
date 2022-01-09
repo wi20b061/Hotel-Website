4c81a74a504032a1035bd0ca28ca86d58eeea3c2
NadiiaGoncharenko <83373973+NadiiaGoncharenko@users.noreply.github.com>
2022-01-09 14:11:02 +0100

    Update show_all_tickets.php

--- show_all_tickets.php
+++ show_all_tickets.php
@@ -1,15 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <title>All tickets</title>
+<?php
+include 'webstructure/head.php';
+?>
+    <title>All Tickets</title>
 </head>
 <body>
+  <?php
+    include 'webstructure/nav.php';
+  ?>
+    <br>
+    <div class="container">
 
-    <?php
+
         require_once ('dbaccess.php');
 
         $db_obj = new mysqli($host, $user, $password, $database);
@@ -51,3 +52,7 @@
 
 </body>
 </html>
+
+<?php
+    include 'webstructure/footer.php';
+?>

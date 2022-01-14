12fac1da2cadb74eb40ebf9442c08413e48d0249
NadiiaGoncharenko <83373973+NadiiaGoncharenko@users.noreply.github.com>
2022-01-14 15:14:35 +0100

    funktioniert
    Hash und überprüfungen sicd jetzt rausgenommen, bearbeite

--- changePw.php
+++ changePw.php
@@ -1,133 +1,80 @@
 <?php
     include 'webstructure/head.php';
+    include 'webstructure/nav.php';
 ?>
-<head>
-<title>Update your profile</title>
+
+ <?php
+
+    //connect with DB
+    require_once('dbaccess.php');
+
+    $db_obj = new mysqli($host, $user, $password, $database);
+    if ($db_obj->connect_error) {
+        echo "Collection failed!";
+        exit();}
+
+     //update in DB
+     if(isset($_POST["passwordNew"]) && isset($_GET["userID"]) && !empty($_GET["userID"])) { 
+
+       $userID = intval($_GET['userID']);
+ 
+       $sql = "UPDATE `user` SET `password`=?  WHERE `userID`=?";   
+ 
+       $password = $_POST["passwordNew"];
+     
+       //use prepare function
+        $stmt = $db_obj->prepare($sql);
+        $stmt->bind_param("si", $password, $userID );
+    
+        //executes the statement
+        $stmt->execute();
+        
+        //close the statement
+        $stmt->close();
+    
+        //close the connection
+        $db_obj->close();
+        header('Location: register.php');
+        die();
+     }
+
+?>
+
+
+<title>Update your password</title>
 </head>
 <body>
       
-  <?php
-    include 'webstructure/nav.php';
-  ?>
- 
- <br>
+<br>
 
  <div class="container text-center">
-     <h1 id = "heading-1">Update your profile </h1>
+     <h1 id = "heading-1">Update your password </h1>
 </div>
 <br>
 
- <?php
-    //change button is selected
-    if (isset($_POST["change"])){
-        //old password
-        if ($_POST["passwordAlt"] !== -1 && password_verify($_POST["passwordAlt"], $_SESSION['password'] )) {
-            
-            //verify the new password
-            if (!preg_match("/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d\w\W]{8,}$/",$_POST["passwordNeu"])) {
-                echo '<div class="alert alert-danger alert-dismissible">Minimum eight letters = 1 character and 1 numerical value</div>';
-                $err=true;
-            } else {
-                //password confirmation
-                if ($_POST["passwordNeu"] != $_POST["passwordNeuB"] ) {
-                    echo '<div class="alert alert-danger alert-dismissible">The password is not the same!</div>';
-                } else{
-                    // the password is protected in form of hash output
-                    $hash = password_hash($_POST["passwordNeu"], PASSWORD_BCRYPT);
-                    //the password is passed along into the databank, with the corresponding username
-                    $db->updatePassword($_SESSION['username'],$hash);
-                    $_SESSION['password'] = $hash;
-
-                    //corresponding header is outputted 
-                    header('Location: index.php?site=profil&change=true');
-                }
-            } 
-
-        } else {
-            //if the password is incorrect 
-            $err=true;
-            echo $_SESSION['password'].'<div class="alert alert-danger alert-dismissible">Incorrect Password!</div>';
-
-        }
-       
-    } 
-       //change button is selected
-       if (isset($_POST["change"])){
-                                          
-           //the first name is passed along into the databank, with the corresponding username
-                $db->updateFname($_SESSION['username']);
-                    
-                //corresponding header is outputted 
-                header('Location: index.php?site=profil&change=true');
-                } 
-        
-     
-
-         //change button is selected
-         if (isset($_POST["change"])){
-         
-              //the last name is passed along into the databank, with the corresponding username
-                $db->updateLname($_SESSION['username']);
-                        
-              //corresponding header is outputted 
-                header('Location: index.php?site=profil&change=true');
-                   
-            } 
-?>
-
 <h3 class="text-center">Changing Your Password</h3>
 <div class="sickbg container jumbotron">
-        <form class="px-4 py-3" method="post" action="index.php?site=password" >
+        <form class="px-4 py-3" method="post" action="#" >
             <div class="form-group">
                 <label for="password1">Password</label>
-                <input type="password" name="passwordAlt"  class="form-control" id="password1" placeholder="Your Password">
+                <input type="password" name="passwordOld"  class="form-control" id="password1" placeholder="Your Password">
             </div>
             <div class="form-group">
                 <label for="password2">New Password</label>
-                <input type="password" name="passwordNeu"  class="form-control" id="password2" placeholder="Your new password">
+                <input type="password" name="passwordNew"  class="form-control" id="password2" placeholder="Your new password">
             </div>
             <div class="form-group">
                 <label for="password3">Retype New Password</label>
-                <input type="password" name="passwordNeuB"  class="form-control" id="password3" placeholder="Type in new password again">
+                <input type="password" name="passwordNewB"  class="form-control" id="password3" placeholder="Type in new password again">
             </div>
             <button type="submit" name="change" class="btn btn-primary">Apply Password Changes</button>
 
         </form>
 </div>
 
-<h3 class="text-center">Changing Your Name</h3>
-<div class="sickbg container jumbotron">
-        <form class="px-4 py-3" method="post" action="index.php?site=fname" >
-          
-            <div class="form-group">
-                <label for="fname2">New First Name</label>
-                <input type="fname" name="fnameNew"  class="form-control" id="fname2" placeholder="Your new First Name">
-            </div>
-           
-            <button type="submit" name="change" class="btn btn-primary">Apply First name changes</button>
-
-        </form>
-
-        <form class="px-4 py-3" method="post" action="index.php?site=lname" >
-           
-            <div class="form-group">
-                <label for="lname2">New Last Name</label>
-                <input type="lname" name="lnameNew"  class="form-control" id="lname2" placeholder="Your new Last Name">
-            </div>
-           
-            <button type="submit" name="change" class="btn btn-primary">Apply Last name changes</button>
-
-        </form>
-
-       </div>
-
-
-
 </body>
 <?php
     include 'webstructure/footer.php';
 ?>
-
-
 
 

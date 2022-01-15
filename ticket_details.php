7b3ec21971fdbd9a0647945b4695d2bf69e78804
NadiiaGoncharenko <83373973+NadiiaGoncharenko@users.noreply.github.com>
2022-01-15 21:28:57 +0100

    Add files via upload

---
+++ ticket_details.php
@@ -0,0 +1,72 @@
+<?php
+    include 'webstructure/head.php';
+    include 'webstructure/nav.php';
+?>
+
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                 <h1>Ticket Details</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2"></div>
+                <div class="col-8">
+
+                    <?php
+                        require_once('dbaccess.php');
+
+                        $db_obj = new mysqli($host, $user, $password, $database);
+                        if ($db_obj->connect_error) {
+                            echo "Collection failed!";
+                            exit();
+
+                        }
+
+                        $ticketID = $_GET['ticketID'];
+
+                        $sql = "SELECT ticket.ticketID, ticket.title, ticket.comment, ticket.text, ticket.img, 
+                        ticket.timestamp,ticketstatus.tstatus FROM ticket 
+                        INNER JOIN ticketstatus ON ticket.statusID=ticketstatus.statusID WHERE ticketID = ? ;";
+
+                        $stmt = $db_obj->prepare($sql);
+                        $stmt->bind_param('i',$ticketID);
+
+                        $stmt->execute();
+                        $stmt ->bind_result($ticketID, $title, $comment, $text, $img, 
+                        $timestamp, $statusID);
+
+                       #  if (!$stmt->fetch()) {
+                           # header("Location: table.php");
+                           # die();
+                       # }
+                        ?>
+
+                        <div class="class">
+                            <div class="class-header">
+                                <?php echo $statusID; ?>
+                            </div>
+                            
+                            <div class="card-body">
+                            <img src="<?php echo $img; ?>" class="card-img-top" alt="Ticket image">
+                                <h5 class="card-title"><?php echo $title; ?></h5>
+                                <h6 class="card-subtitle mb-2 text-muted"><?php echo $timestamp; ?></h6>
+                                <a href="table.php" class="btn btn-primary">Overview</a>
+                                <?php #tech=2
+                                    if($_SESSION["userrole"] == 2 ): ?> 
+
+                                    <a href="edit_ticket.php?id= " . $ticketID . "  class="btn btn-primary">Edit</a>
+                                 
+                                    <?php endif ?> 
+
+                            </div>
+                            </div>
+                        </div>
+                    <div class="col-2"></div>
+                </div>
+            </div>
+
+    </body>
+

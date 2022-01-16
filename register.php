fabc65a01ce432c905974dfdf72ad374af1af3a0
NadiiaGoncharenko <83373973+NadiiaGoncharenko@users.noreply.github.com>
2022-01-16 20:59:16 +0100

    congrats
    support Seite - it is Success

---
+++ register.php
@@ -0,0 +1,27 @@
+<?php
+include 'webstructure/head.php';
+?>
+    <title>Registration</title>
+  </head>
+  <body>
+    <?php
+      include 'webstructure/nav.php';
+    ?>
+    <br>
+
+    <h1 class="page-header text-center">Congratulations</h1><br/>
+    <div class="container">
+      <div class="space">
+      <p class="text-center">
+      
+      <br>
+      <br>
+      <h2 class="page-header text-center"> It is a success!</h2><br/>
+         
+      </p>
+      </div>
+
+
+    <?php
+  include 'webstructure/footer.php';
+?>

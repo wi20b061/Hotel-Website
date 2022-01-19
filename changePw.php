21468534ff9e37afb4ce1580620cbeec168a12d8
NadiiaGoncharenko <83373973+NadiiaGoncharenko@users.noreply.github.com>
2022-01-19 21:28:23 +0100

    change PW korrigiert+editData Link angepasst

--- changePw.php
+++ changePw.php
@@ -16,47 +16,52 @@
         echo "Collection failed!";
         exit();
     }
-?>
 
-<?php
     //update in DB
-    if(isset($_POST["passwordNew"]) && !empty($_POST["passwordNew"]) && isset($_GET["userID"]) && !empty($_GET["userID"]) && isset($_SESSION["userID"])) { 
+    if(isset($_POST["passwordNew"]) && !empty($_POST["passwordNew"]) && isset($_GET["userID"]) 
+    && !empty($_GET["userID"]) && isset($_SESSION["userID"])) { 
         //get pw from db
-        //$userID = '7'; #intval($_POST['userID']);
-        $userID = $_SESSION['userID'];  //read userID from Session
+        //$userID = '7'; //simmulation
+       // $userID = $_SESSION['userID'];  //read userID from Session
 
         $request = "SELECT user.password FROM user WHERE user.userID = ?";
+
+        //use prepared stmt
         $stmt = $db_obj->prepare($request);
-        $stmt->bind_param("s", $pw );
+        $stmt->bind_param("s", $_GET["userID"] );
+
+        //execute stmt
         $stmt->execute();
-        echo $pw;
 
+        $stmt->bind_result($pw);
+        $stmt->fetch();
+        
+               
         // the password is protected in form of hash output
-        $hash = password_hash($_POST["passwordOld"], PASSWORD_BCRYPT);
+      //  $hash = password_hash($_POST["passwordOld"], PASSWORD_BCRYPT);
 
-        //change button is selected
-        # if (isset($_POST["change"])){
         //old password check
-        if (password_verify($_POST["passwordOld"], $pw)) {
+       if (password_verify($_POST["passwordOld"], $pw)) {
+      
+            //close the statement
+            $stmt->close();
 
             //password confirmation
             if ($_POST["passwordNew"] != $_POST["passwordNewB"] ) {
-                echo '<div class="alert alert-danger alert-dismissible">The password is not the same!</div>';
+                echo '<p class="error">The password is not the same!</p>';
             }
             //verify the new password
             /*if (!preg_match("/^(?=.?[A-Z])(?=.?[a-z])(?=.?[0-9])(?=.?[#?!@$%^&*-]).{8,}$/",$_POST["passwordNew"])) {
                 echo '<div class="alert alert-danger alert-dismissible">Minimum four letters = 1 character and 1 numerical value</div>';
                 $err=true;
-            }
-            else{*/
+            }*/
+            else{
                 // the password is protected in form of hash output
                 $hash = password_hash($_POST["passwordNew"], PASSWORD_BCRYPT);
 
                 $userID = intval($_GET['userID']);
                 $sql = "UPDATE `user` SET `password`=?  WHERE `userID`=?";   
-            
-                # $password = $_POST["passwordNew"];
-                
+                                         
                 //use prepare function
                 $stmt = $db_obj->prepare($sql);
                 $stmt->bind_param("si", $hash, $userID );
@@ -74,12 +79,12 @@
                 $stmt->close();
                 //close the connection
                 $db_obj->close();
-            //}
+            }
         }else{
-            $error ="Try again";
-            die("Old passwort is wrong");
+            echo '<div class="alert alert-danger alert-dismissible">Old passwort is wrong</div>';
+           
         }
-    }
+   }
 ?>
 
 
